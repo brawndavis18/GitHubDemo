cf43b146e51a137460a4c1911e380c3669cedb62
ivanp <ivanp@Aizen>
2024-01-09 14:26:34 -0500

    help

--- Prime/src/Prime.java
+++ Prime/src/Prime.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class Prime {
+	   public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter a number to find all prime numbers less than it: ");
+	        int limit = scanner.nextInt();
+
+	        System.out.println("Prime numbers less than " + limit + ":");
+	        printPrimeNumbers(limit);
+
+	        scanner.close();
+	    }
 
 }
